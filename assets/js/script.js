3cfce6969e47aa035db3ec34711e8696e1432fc0
Sophie Dufrane <dufranesophie@gmail.com>
2024-08-25 19:08:10 +0000

    Add function checkWinner and incrementScore to built next steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,55 @@
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            let userChoice = this.getAttribute("data-choice");
-            runGame(userChoice);
+            let userSelection = this.getAttribute("data-selection");
+            runGame(userSelection);
         });
     }
 });
 
-const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-const userImage = document.getElementById("user-selection");
-const computerImage = document.getElementById("computer-selection");
 
-function runGame(userChoice) {
+// Declare variable for DOM elements
+const selection = ["rock", "paper", "scissors", "lizard", "spock"];
+const userIcon = document.getElementById("user-selection");
+const computerIcon = document.getElementById("computer-selection");
+
+/**
+ * Main game funtion, with one parameter: the user choice
+ * which is the data-choice of the button selected (Rock, paper, scissors...)  
+ */
+function runGame(userSelection) {
+	 
+    // Update the user's selected image
+    userIcon.src = `assets/images/${selection[userSelection]}.png`;
+	userIcon.alt = selection[userSelection];
 	
-	userImage.src = `assets/images/${choices[userChoice]}.png`;
-	userImage.alt = choices[userChoice];
+     // Get the computer selecyion by creation a random number between 0 and 4
+	let computerSelection = Math.floor(Math.random() * 5);
 	
-	let computerChoice = Math.floor(Math.random() * 5);
+    // Update the computer's selected image
+	computerIcon.src = `assets/images/${selection[computerSelection]}.png`;
+	computerIcon.alt = selection[computerSelection];
 	
-	computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-	computerImage.alt = choices[computerChoice];
-	
-	let result = checkWinner(choices[playerChoice], choices[computerChoice]);
+    // check result with the function checkWinner with 2 variable, the user and computer selection.
+	let result = checkWinner(selection[userSelection], selection[computerSelection]);
 	
 	updateScores(result);
+}
+
+
+function checkWinner (userSelection, computerSelection) {
+    if (userSelection === computerSelection) {
+        alert ("Draw!")
+    } else {
+        alert ("Looser!")
+    }
+    
+}
+
+
+function incrementScore() {
+
 }
