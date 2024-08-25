42ca65a487a7c06eaa5e520c17f00bd152e6d526
Sophie Dufrane <dufranesophie@gmail.com>
2024-08-25 08:29:31 +0000

    Add data-choice to buttons in index.html and add first function with event listener to get the user choice when a button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("connected!");
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let choice = this.getAttribute("data-choice");
+
+            if (choice === "0") {
+                alert("You selected Rock");
+            } else if (choice === "1") {
+                alert("You selected Paper");
+            } else if (choice === "2") {
+                alert("You selected Scissors");
+            } else if (choice === "3") {
+                alert("You selected Lizard");
+            } else if (choice === "4") {
+                alert("You selected Spock");
+            }
+        });
+    }
+});
+
